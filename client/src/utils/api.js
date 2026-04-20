527f403a1652c46e8ee1e639c07a8c194ce2f693
harsh-codes12 <hrsh.er@icloud.com>
2026-04-20 23:58:22 +0530

    fixed api route

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://devconnect-backend-ztun.onrender.com",
+  baseURL: "baseURL: "https://devconnect-backend-ztun.onrender.com/api"",
 });
 
 
